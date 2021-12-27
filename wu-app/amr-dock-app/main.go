ca4a600ffe6c74a2678792243c59dedabb94d587
wuzhikun <shengdawuzk@163.com>
2021-12-27 16:17:07 +0800

    add wu-app

---
+++ wu-app/amr-dock-app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"gitee.com/sienectagv/gozk/zsql"
+	"gitee.com/sienectagv/gozk/zutils"
+	"github.com/nurozhikun/zkgo-ims/zk-planet/zipamr"
+)
+
+func main() {
+	waitGroup := zutils.NewLoopGroup()
+	amr := &zipamr.ZipAmr{}
+	//init db
+	cfg := &zsql.Cfg{
+		Type:     zsql.TypeSqlite,
+		Database: "user/map.db",
+	}
+	amr.InitDB(cfg).
+		InitDevs()
+	waitGroup.WaitForEnter("quit")
+	amr.FreeAll()
+}
